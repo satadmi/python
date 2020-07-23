bd12c884d4bf83eca1ddf002cf41b155191e3fa5
sat <421155501a@gmail.com>
2020-07-23 00:22:45 -0700

    with netconf

--- netconf.py
+++ netconf.py
@@ -4,4 +4,7 @@ router = {"host": "ios-xe-mgmt.cisco.com", "port": "10000",
 "username": "developer", "password": "Cisco12345"}
 
 with manager.connect(host=router["host"], port=router["port"], username=router["username"], password=router["password"], hostkey_verify=False) as m:
+    for capability in m.server_capabilities:
+        print('*' * 50)
+        print(capability)
     m.close_session()
